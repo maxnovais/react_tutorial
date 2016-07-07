792f4947f9e156a150e8e45bbe3650497f57e716
Maxwell <maxnovais@gmail.com>
2016-07-07 16:51:12 -0300

    Added classes for comment list and form

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,14 +1,35 @@
-// tutorial1-raw.js
-var CommentBox = React.createClass({displayName: 'CommentBox',
+// tutorial1.js
+var CommentBox = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: "commentBox"},
-        "Hello, world! I am a CommentBox."
-      )
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
     );
   }
 });
 ReactDOM.render(
-  React.createElement(CommentBox, null),
+  <CommentBox />,
   document.getElementById('content')
 );
+
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
